d4d7cf9391c9139f229e7e5fe81cfb153d462814
felkj <felipep1424@gmail.com>
2025-02-25 15:06:31 -0300

    Primeiro Commit pelo IntelliJ Implementação da camada de dominio

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -2,7 +2,6 @@ package me.dio.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import me.dio.domain.model.User;
 
 @Repository
