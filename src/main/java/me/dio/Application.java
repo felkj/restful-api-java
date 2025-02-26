1e26b07e605b15ed7d2a2f6a910033d69afed5f6
Felipe Silva <96023311+felkj@users.noreply.github.com>
2025-02-26 15:26:01 -0300

    Update Application.java

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,6 +1,7 @@
 package me.dio;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
